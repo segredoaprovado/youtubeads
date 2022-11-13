46531c8dd7da3bb4794b1c199fe283a6a6297798
segredoaprovado <109923826+segredoaprovado@users.noreply.github.com>
2022-11-13 01:10:45 -0300

    upzuck1
    upzuck1

---
+++ scriptdecompradores/js/lobibox-content.js
@@ -0,0 +1,151 @@
+var IMG_PREFIX = 'https://i.ibb.co/fQfpypM/';
+//LobiBox default options
+Lobibox.notify.DEFAULTS = $.extend({}, Lobibox.notify.DEFAULTS, {
+    sound: false,
+    position: 'bottom left',
+    title: false,
+    delay: 5000,
+    messageHeight: 90,
+});
+(function() {
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Andressa G</b> acabou de entrar. Restam <b>menos de 100 vagas</b>!'
+        });
+    }, 1712000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 6 pessoas entraram nos últimos 15 minutos.</b>  <br><br><b>Clique</b> e garanta também a sua vaga...'
+        });
+    }, 1720000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gabriel J</b> acabou de entrar.</br> Restam <b>menos de 100 vagas</b>!'
+        });
+    }, 1728000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram de entrar... </b>.<br><br><b>Clique</b> e garanta a sua vaga agora...'
+        });
+    }, 1736000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Guilherme E</b> acabou de entrar. Restam <b>menos de 100 vagas</b>!'
+        });
+    }, 1744000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Aléxia E</b> acabou de entrar.  Restam <b>menos de 80 vagas!</b>'
+        });
+    }, 1752000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Gabriel M</b> acabou de entrar.  Restam <b>poucas vagas</b>!'
+        });
+    }, 1760000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 novas pessoas acabaram de entrar no Close Friends da Aprovação </b>.<br><br><b>Garanta a sua vaga agora</b>...'
+        });
+    }, 1768000);
+
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Camilo Santos</b> acabou entrar.<br><br><b>Clique agora</b> e garanta a sua vaga....'
+        });
+    }, 1776000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Rodrigo T</b> acabou de entrar.<br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1784000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram o Close Friends da Aprovação nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1792000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>David J</b> acabou de entrar. Restam <b>menos de 70 vagas!</b>'
+        });
+    }, 1800000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 6 pessoas compraram o Close Friends da Aprovação.</b>  <br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1808000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Rafaela E</b> acabou de entrar.</br> Restam <b>menos de 70 vagas!</b>'
+        });
+    }, 1816000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>4 pessoas acabaram comprar o Close Friends da Aprovação </b>.<br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1824000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Mariana E</b> acabou de entrar. Restam <b>menos de 70 vagas!</b>'
+        });
+    }, 1832000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Rafaela E</b> acabou de comprar o Close Friends da Aprovação.  Restam <b>poucas vagas!</b>'
+        });
+    }, 1840000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Juliana E</b> acabou de entrar.  Restam <b>menos de 100 vagas!</b>'
+        });
+    }, 1848000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>2 novas pessoas acabaram de comprar o Close Friends da Aprovação </b>.<br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1856000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Guilherme E</b> acabou de entrar.<br><br><b>Clique</b> e garanta a sua vaga...'
+        });
+    }, 1864000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Adriano E</b> acabou de entrar.<br><br><b>Clique</b> e garanta a sua vaga agora...'
+        });
+    }, 1872000);
+    setTimeout(function() {
+        Lobibox.notify('info', {
+            img: IMG_PREFIX + '3-potes.png',
+            msg: '<b>Novas 5 pessoas compraram o Close Friends da Aprovação  nos últimos 10 minutos</b>.<br><br><b>Clique</b> e garanta também a sua vaga...'
+        });
+    }, 1880000);
+})();
